850802dc71277edef9a428f7d4385e34a7d0d428
dineshbalachandran <dinesh_k_b@hotmail.com>
2020-05-20 19:04:46 +1000

    add package component

--- app/src/app/package/package-routing.module.ts
+++ app/src/app/package/package-routing.module.ts
@@ -2,9 +2,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
 
 import { PackageListComponent } from './package-list/package-list.component';
+import { PackageComponent } from './package/package.component';
 
 const routes: Routes = [
-    {path: '', component: PackageListComponent, //implementing lazy loading
+    {
+        path: '', component: PackageComponent, //implementing lazy loading
+        children: [
+            {path: '', component: PackageListComponent}            
+        ]
     }
 ]
 
--- app/src/app/package/package.module.ts
+++ app/src/app/package/package.module.ts
@@ -6,11 +6,21 @@ import { PackageRoutingModule } from './package-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatTabsModule } from '@angular/material/tabs';
+import { PackageComponent } from './package/package.component';
 
 @NgModule({
     declarations: [
-          PackageListComponent   
+          PackageListComponent,
+          PackageComponent   
     ],
-    imports: [PackageRoutingModule, SharedModule, MatTableModule, MatPaginatorModule, MatSortModule]
+    imports: [
+        PackageRoutingModule, 
+        SharedModule, 
+        MatTabsModule, 
+        MatTableModule, 
+        MatPaginatorModule, 
+        MatSortModule
+    ]
 })
 export class PackageModule {}
